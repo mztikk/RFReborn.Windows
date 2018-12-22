7b7e8b4aec3eab016f933cac2c89051adb5285b0
mztikk <nevigskcuforez@gmail.com>
2018-12-22 19:26:51 +0100

    Add project files.

---
+++ RFReborn.Windows/Extensions/ProcessExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Management;
+
+namespace RFReborn.Windows.Extensions
+{
+    /// <summary>
+    /// Extends <see cref="Process"/>
+    /// </summary>
+    public static class ProcessExtensions
+    {
+        /// <summary>
+        /// Gets all commandline arguments a process was started with.
+        /// </summary>
+        /// <param name="proc">Process to get arguments from.</param>
+        /// <returns>String of the commandline arguments if successfull, <see cref="string.Empty"/> otherwise.</returns>
+        public static string GetCommandLine(this Process proc)
+        {
+            using (var searcher = new ManagementObjectSearcher(
+              $"SELECT CommandLine FROM Win32_Process WHERE ProcessId = {proc.Id}"))
+            {
+                foreach (var item in searcher.Get())
+                {
+                    var cmd = item["CommandLine"];
+                    if (cmd == null)
+                    {
+                        continue;
+                    }
+
+                    return cmd.ToString();
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
