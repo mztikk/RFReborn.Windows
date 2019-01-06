7490aca75c0f08b42bc7e1dfd0314bdb0be0eb61
mztikk <nevigskcuforez@gmail.com>
2019-01-06 19:47:55 +0100

    added Process.GetModule

--- RFReborn.Windows/Extensions/ProcessExtensions.cs
+++ RFReborn.Windows/Extensions/ProcessExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Management;
 
 namespace RFReborn.Windows.Extensions
@@ -31,6 +32,26 @@ namespace RFReborn.Windows.Extensions
             }
 
             return string.Empty;
+        }
+
+        /// <summary>
+        /// Searches the <see cref="ProcessModuleCollection"/> for the specified name.
+        /// </summary>
+        /// <param name="proc"><see cref="Process"/> which modules to search</param>
+        /// <param name="moduleName">Module name to search for.</param>
+        /// <returns><see cref="ProcessModule"/></returns>
+        public static ProcessModule GetModule(this Process proc, string moduleName)
+        {
+            for (int i = 0; i < proc.Modules.Count; i++)
+            {
+                ProcessModule module = proc.Modules[i];
+                if (module.ModuleName.Equals(moduleName, StringComparison.Ordinal))
+                {
+                    return module;
+                }
+            }
+
+            throw new DllNotFoundException($"Module \"{moduleName}\" not found.");
         }
     }
 }
