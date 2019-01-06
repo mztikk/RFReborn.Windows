c9a5953b479c5fd052b8b7c990526d15fa7ec332
mztikk <nevigskcuforez@gmail.com>
2019-01-06 09:10:26 +0100

    added User32.SendMessage

---
+++ RFReborn.Windows/Native/User32.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RFReborn.Windows.Native
+{
+    /// <summary>
+    /// Methods from the User32.dll
+    /// </summary>
+    public static class User32
+    {
+        /// <summary>
+        ///     Sends the specified message to a window or windows. The SendMessage function calls the window procedure for the
+        ///     specified window and does not return until the window procedure has processed the message.
+        ///     To send a message and return immediately, use the SendMessageCallback or SendNotifyMessage function.To post a
+        ///     message to a thread's message queue and return immediately, use the PostMessage or PostThreadMessage function.
+        /// </summary>
+        /// <param name="hWnd">
+        ///     A handle to the window whose window procedure will receive the message. If this parameter is HWND_BROADCAST
+        ///     ((HWND)0xffff), the message is sent to all top-level windows in the system, including disabled or invisible unowned
+        ///     windows, overlapped windows, and pop-up windows; but the message is not sent to child windows.
+        ///     Message sending is subject to UIPI.
+        ///     The thread of a process can send messages only to message queues of threads in
+        ///     processes of lesser or equal integrity level.
+        /// </param>
+        /// <param name="uMsg">
+        ///     The message to be sent.
+        /// </param>
+        /// <param name="wParam">
+        ///     Additional message-specific information.
+        /// </param>
+        /// <param name="lParam">
+        ///     Additional message-specific information.
+        /// </param>
+        /// <returns>
+        ///     The return value specifies the result of the message processing; it depends on the message sent.
+        /// </returns>
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int SendMessage([In] IntPtr hWnd, [In] int uMsg, [In] uint wParam, [In] uint lParam);
+    }
+}
