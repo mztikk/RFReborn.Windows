0293a0e5b5a4d640b9ef6c449aa8db20d8122b1c
mztikk <nevigskcuforez@gmail.com>
2020-08-24 21:31:23 +0200

    added Test project and RemoteMemoryWriteBytesTests

---
+++ RFReborn.Windows.Tests/RemoteMemoryTests/RemoteMemoryBaseTest.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+namespace RFReborn.Windows.Tests.RemoteMemoryTests
+{
+    public class RemoteMemoryBaseTest
+    {
+        public Process GetProcess() => Process.GetCurrentProcess();
+    }
+}
---
+++ RFReborn.Windows.Tests/RemoteMemoryTests/WriteBytesTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Windows.Memory;
+
+namespace RFReborn.Windows.Tests.RemoteMemoryTests
+{
+    [TestClass]
+    public unsafe class WriteBytesTests : RemoteMemoryBaseTest
+    {
+        [TestMethod]
+        public void WriteBytes()
+        {
+            byte[] bytes = new byte[] { 1, 2, 3 };
+            byte[] preBytes = new byte[bytes.Length];
+            byte[] newBytes = new byte[] { 4, 5, 6 };
+            bytes.CopyTo(preBytes, 0);
+            using (var memory = new RemoteMemory(GetProcess()))
+            {
+                CollectionAssert.AreEqual(bytes, preBytes);
+                fixed (byte* bp = bytes)
+                {
+                    memory.WriteBytes(new System.IntPtr(bp), newBytes);
+                }
+                CollectionAssert.AreNotEqual(preBytes, bytes);
+                CollectionAssert.AreEqual(newBytes, bytes);
+            }
+        }
+    }
+}
