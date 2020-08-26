dd0c3f50fa40154a3d478a247a8c110c84853d88
mztikk <nevigskcuforez@gmail.com>
2020-08-26 19:55:35 +0200

    added ReadTests with int and float

---
+++ RFReborn.Windows.Tests/RemoteMemoryTests/Read/ReadTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFReborn.Windows.Memory;
+
+namespace RFReborn.Windows.Tests.RemoteMemoryTests.Read
+{
+    [TestClass]
+    public unsafe class ReadTests : RemoteMemoryBaseTest
+    {
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(1781818)]
+        [DataRow(-1781818)]
+        [DataRow(int.MaxValue)]
+        [DataRow(int.MinValue)]
+        public void ReadInt(int input)
+        {
+            using (var memory = new RemoteMemory(GetProcess()))
+            {
+                Assert.AreEqual(input, memory.Read<int>(new IntPtr(&input)));
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow(0f)]
+        [DataRow(178.1818f)]
+        [DataRow(-178.1818f)]
+        [DataRow(float.MaxValue)]
+        [DataRow(float.MinValue)]
+        public void ReadFloat(float input)
+        {
+            using (var memory = new RemoteMemory(GetProcess()))
+            {
+                Assert.AreEqual(input, memory.Read<float>(new IntPtr(&input)));
+            }
+        }
+    }
+}
