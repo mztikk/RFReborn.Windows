a179cb6dd601f3ffb328afced8e352186176d115
Nils Feierabend <nevigskcuforez@gmail.com>
2021-02-23 22:21:56 +0100

    added openprocess exception

---
+++ RFReborn.Windows/Memory/Exceptions/OpenProcessException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RFReborn.Windows.Memory.Exceptions
+{
+    public class OpenProcessException : Exception
+    {
+        public OpenProcessException() : base("Failed to open process")
+        {
+        }
+
+        public OpenProcessException(string message) : base(message)
+        {
+        }
+
+        public OpenProcessException(int pid) : base(GetMessageForProcess(pid))
+        {
+        }
+
+        public OpenProcessException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public OpenProcessException(int pid, Exception innerException) : base(GetMessageForProcess(pid), innerException)
+        {
+        }
+
+        protected OpenProcessException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        private static string GetMessageForProcess(int pid) => $"Couldn't open process: {pid}";
+    }
+}
